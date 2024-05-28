e5333c66605924be90951a0972226d16e7ece4ea
mihailsadovnikov <mihailsadovnikov08@gmail.com>
2024-05-28 17:03:22 +0400

    Старт

---
+++ ie_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ai
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string[] questions = { "привет", "как тебя зовут" };
+            string[] answers = { "привет", "Доума ",
+            };
+            string question = "";
+
+            do
+            {
+                question = Console.ReadLine();
+
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    if (questions[i] == question)
+                    {
+                        Console.WriteLine(answers[i]);
+
+                        break;
+                    }
+                    else if (i == questions.Length - 1)
+                    {
+                        Console.WriteLine("Гай знает я не знаю!!!");
+                    }
+
+                }
+
+
+            } while (question != "пока");
+
+
+
+
+
+
+        }
+    }
+}
