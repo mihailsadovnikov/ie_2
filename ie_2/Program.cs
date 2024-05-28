a0953f165631ae36bbcdeee128a81e323055226d
mihailsadovnikov <mihailsadovnikov08@gmail.com>
2024-05-28 17:33:05 +0400

    добавили обучение

--- ie_2/Program.cs
+++ ie_2/Program.cs
@@ -13,16 +13,25 @@ namespace ai
 
         static void Main(string[] args)
         {
-            string[] questions = { "привет", "как тебя зовут" };
-            string[] answers = { "привет", "Доума ",
-            };
-            string question = "";
+           // string[] questions = { "привет", "как тебя зовут" };
+           List<string> questions = new List<string>();
+            questions.Add("привет");
+            questions.Add("как тебя зовут");
+
+            List<string> answers = new List<string>();
+            // string[] answers = { "привет", "Доума ",
+            //};
+            answers.Add("привет");
+            answers.Add("Доума ");
+
+
+            string question = "\"Доума \"";
 
             do
             {
                 question = Console.ReadLine();
 
-                for (int i = 0; i < questions.Length; i++)
+                for (int i = 0; i < questions.Count; i++)
                 {
                     if (questions[i] == question)
                     {
@@ -30,9 +39,15 @@ namespace ai
 
                         break;
                     }
-                    else if (i == questions.Length - 1)
+                    else if (i == questions.Count - 1)
                     {
                         Console.WriteLine("Гай знает я не знаю!!!");
+                        questions.Add(question);
+                        string answer = Console.ReadLine();
+
+                        answers.Add(answer);
+                        Console.WriteLine("понял");
+
                     }
 
                 }
